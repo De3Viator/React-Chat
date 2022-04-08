703d49eeec2003908b668e99c984756e1548eeb5
De3Viator <yurasadf2@gmail.com>
2022-04-08 15:08:49 +0300

    init

--- homework/src/components/classes/Message.jsx
+++ homework/src/components/classes/Message.jsx
@@ -1,8 +1,14 @@
 import { Component } from "react";
-import '../../Message.css'
+import "../../Message.css";
 
 export class Message extends Component {
-    render() {
-       return  <input type="text" value={this.props.text} onChange={this.props.changeValue}/>
-    }
+  render() {
+    return (
+      <input
+        type="text"
+        value={this.props.text}
+        onChange={this.props.changeValue}
+      />
+    );
+  }
 }
