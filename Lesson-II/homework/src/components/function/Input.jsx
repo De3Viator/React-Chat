f8af05bcce7581561485ac301707116104c19958
De3Viator <yurasadf2@gmail.com>
2022-04-09 22:35:22 +0300

    added husky

--- Lesson-II/homework/src/components/function/Input.jsx
+++ Lesson-II/homework/src/components/function/Input.jsx
@@ -1,6 +1,7 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 export default function Input(props) {
   const message = useRef(null);
+  const [test,setTest] = useState(0);
   function setValue() {
     props.changeValue(message.current.value, props.element);
   }
