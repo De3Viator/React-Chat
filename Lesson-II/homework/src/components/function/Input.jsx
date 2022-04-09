0916a52daabdd9f424376c4e99a029e46d17c4d2
De3Viator <yurasadf2@gmail.com>
2022-04-09 22:36:09 +0300

    check husky

--- Lesson-II/homework/src/components/function/Input.jsx
+++ Lesson-II/homework/src/components/function/Input.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from 'react';
 export default function Input(props) {
   const message = useRef(null);
-  const [test,setTest] = useState(0);
   function setValue() {
     props.changeValue(message.current.value, props.element);
   }
