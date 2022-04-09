0dfaa15ca7a00cb3e2375a8871c1d20f7194e862
De3Viator <yurasadf2@gmail.com>
2022-04-09 22:27:49 +0300

    add eslint

--- Lesson-II/homework/src/App.jsx
+++ Lesson-II/homework/src/App.jsx
@@ -1,55 +1,63 @@
-import React ,{ useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import Button from './components/function/Button';
 import Input from './components/function/Input';
 
 function App() {
   const [messageList, setMessageList] = useState([]);
-  const [name,setName] = useState('');
-  const [message,setMessage] = useState('');
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
   const formAction = useRef(null);
-  const [send,setSend] = useState(false);
+  const [send, setSend] = useState(false);
 
-  useEffect( () => {
-    if(send === true) {
+  useEffect(() => {
+    if (send === true) {
       const botAnswer = {
-        name:'bot',
-        message:'Ваше сообщение было отправлено!'
-      }
+        name: 'bot',
+        message: 'Ваше сообщение было отправлено!',
+      };
       setTimeout(() => {
-        setMessageList(prevValue => [...prevValue,botAnswer]);
-      },1500);
+        setMessageList((prevValue) => [...prevValue, botAnswer]);
+      }, 1500);
     }
-  },[send])
+  }, [send]);
 
-  function changeValue(value,changeElement) {
-    if(changeElement === 'message') {
+  function changeValue(value, changeElement) {
+    if (changeElement === 'message') {
       setName(value);
-    } else if(changeElement === 'name') {
+    } else if (changeElement === 'name') {
       setMessage(value);
     }
   }
 
   function addMessage() {
     setSend(true);
-    setMessageList(prevValue => [...prevValue,{name,message}]);
+    setMessageList((prevValue) => [...prevValue, { name, message }]);
     setTimeout(() => {
-      setSend(false)
-    }, 2000)
+      setSend(false);
+    }, 2000);
   }
 
   return (
     <>
-    <form className={send ? 'sended' : 'notSended'} ref={formAction} action='#'>
-      <Button addMessage={addMessage}/>
-      Name:<Input changeValue={changeValue} element={'message'}/>
-      Message<Input message={message} changeValue={changeValue} element={'name'}/>
-    </form>
-    <ul>
-      {messageList.map((message) => (
-          <li>{message.name}:{message.message}</li>
-      ))}
-    </ul>
+      <form
+        className={send ? 'sended' : 'notSended'}
+        ref={formAction}
+        action="#"
+      >
+        <Button addMessage={addMessage} />
+        Name:
+        <Input changeValue={changeValue} element={'message'} />
+        Message
+        <Input message={message} changeValue={changeValue} element={'name'} />
+      </form>
+      <ul>
+        {messageList.map((message) => (
+          <li key={message.message}>
+            {message.name}:{message.message}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
