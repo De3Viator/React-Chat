3f387b34579f7c9a4374339d4cffd1b5fddedb90
De3Viator <yurasadf2@gmail.com>
2022-04-10 21:56:01 +0300

    added storybook

--- Lesson-II/homework/src/stories/Input.stories.jsx
+++ Lesson-II/homework/src/stories/Input.stories.jsx
@@ -1,40 +1,29 @@
 import React from 'react';
+import { Input } from './Input';
 
-import { Button } from './Button';
-
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
-  component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  title: 'Example/Input',
+  component: Input,
   argTypes: {
     backgroundColor: { control: 'color' },
   },
 };
-
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = (args) => <Button {...args} />;
-
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
+const Template = (args) => <Input {...args} />;
 
 export const Large = Template.bind({});
 Large.args = {
   size: 'large',
-  label: 'Button',
+  fontSize: 'large',
 };
 
 export const Small = Template.bind({});
 Small.args = {
   size: 'small',
-  label: 'Button',
+  fontSize: 'small',
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  size: 'medium',
+  fontSize: 'medium',
 };
