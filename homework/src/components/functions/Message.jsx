960343fd836bc5ea9ddf50e2d02a203f39c94e6b
De3Viator <yurasadf2@gmail.com>
2022-04-12 15:18:50 +0300

    added specs

--- homework/src/components/functions/Message.jsx
+++
@@ -1,6 +0,0 @@
-import { useState } from "react";
-import '../../Message.css'
-
-export function Message(props ) {
-    return (<input type="text" value={props.text} onChange={props.changeValue}/>)  
-}
