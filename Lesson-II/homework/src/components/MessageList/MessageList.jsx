b4ed49d2b166a87b4f2d12f9e6acab9085e73862
De3Viator <yurasadf2@gmail.com>
2022-04-13 18:13:53 +0300

    add all tests

--- Lesson-II/homework/src/components/MessageList/MessageList.jsx
+++ Lesson-II/homework/src/components/MessageList/MessageList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-export function MessageList(props) {
+// eslint-disable-next-line no-unused-vars
+export function MessageList({ props, handleMessages }) {
   return (
     <ul>
-      {props.messageList.map((message, index) => (
-        <li key={index}>
+      {handleMessages.map((message, index) => (
+        <li key={index} data-testid="message">
           {message.name}:{message.message}
         </li>
       ))}
